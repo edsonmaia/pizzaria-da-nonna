b11c8218ce9d8495e7fd1ed0ad3effd4b64b2aa4
Edson Maia <edsonmaia2005@hotmail.com>
2022-02-15 15:35:34 -0300

    Aula 02
    Exibir lista de pizzas

---
+++ pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:25.00, sizes:['100g', '530g', '860g'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:26.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:28.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
+    {id:4, name:'Brasileira', img:'images/pizza4.png', price:30.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:29.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:35.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
+    {id:7, name:'Banana com canela', img:'images/pizza7.png', price:32.00, sizes:['320g', '530g', '860g'], description:'Mussarela, banana, canela e açúcar'},
+    {id:8, name:'Chocolate com morango', img:'images/pizza8.png', price:35.00, sizes:['320g', '530g', '860g'], description:'Creme de leite, lascas de chocolate e morangos'}
+];
