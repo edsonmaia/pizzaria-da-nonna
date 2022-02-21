9fed48d899ab7e8ce5301fdaa762ad22b38e8032
Edson Maia <edsonmaia2005@hotmail.com>
2022-02-21 19:14:30 -0300

    Aula 03
    Aula 03

--- js/script.js
+++ js/script.js
@@ -10,4 +10,28 @@ pizzaJson.map((item, index ) => {
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
 
+    // pizza clicada
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log('Clicou na pizza')
+
+        // abrir janela modal
+        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+
+        // preenchimento dos dados
+        document.querySelector('.pizzaBig img').src = item.img
+        document.querySelector('.pizzaInfo h1').innerHTML = item.name
+        document.querySelector('.pizzaInfo--desc').innerHTML = item.description
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+
+    })
+
+    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
+
+    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
+
 })
