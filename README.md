b2eef1e4bf77ccd519d5472c695d447a24131658
Edson Maia <edsonmaia2005@hotmail.com>
2022-02-23 11:05:05 -0300

    Aula 06
    Aula 06

---
+++ README.md
@@ -0,0 +1,62 @@
+# Pizzaria da Nonna
+
+## Apresentação
+Aplicação responsiva desenvolvida em HTML5, CSS3 e JavaScript puro.
+Utilizando recursos modernos da linguagem.
+
+## Aula 01
+* Apresentação geral do projeto
+* Conceito inicial
+* Principais funcionalidades
+* Mais outras podem ser desenvolvidas
+* Aula 01 no YouTube
+<https://youtu.be/Sv2Qcj-9pjI>
+
+## Aula 02
+* Programação para exibição da lista de pizzas
+* JSON, JSON Viewer
+* map, cloneNode, append
+* Aula 02 no YouTube
+<https://youtu.be/7mnXa1jHOMU>
+
+## Aula 03
+* Janela Modal
+* Preencher dados na Janela Modal
+* document.querySelector, addEventListener
+* click, event, preventDefault
+* style.display, style, display, flex, none
+* src
+* innerHTML
+* Aula 03 no YouTube
+<https://youtu.be/3_8gCSI7NNA>
+
+## Aula 04
+* Funções Úteis ou Auxiliares
+* arrow functions
+* encapsular funcionalidades
+* modularizar o código
+* passagem de parâmetros
+* forEach
+* Aula 04 no YouTube
+<https://youtu.be/7XIyH-rukrU>
+
+## Aula 05
+* Tamanhos e Preços na Janela Modal
+* Identificar modal ativa
+* forEach
+* Preencher detalhes dos tamanhos
+* Escolher preço por tamanho
+* Formatações monetárias
+* Mudar quantidades com botões
+* Aula 05 no YouTube
+<https://youtu.be/NkPSqYK_uRQ>
+
+## Aula 06
+* Carrinho de Compras
+* Aula 06 no YouTube
+<https://youtu.be/tczhRdyj1nM>
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -117,6 +117,179 @@ const mudarQuantidade = () => {
 }
 // /aula 05
 
+// aula 06
+const adicionarNoCarrinho = () => {
+    seleciona('.pizzaInfo--addButton').addEventListener('click', () => {
+        console.log('Adicionar no carrinho')
+
+        // pegar dados da janela modal atual
+    	// qual pizza? pegue o modalKey para usar pizzaJson[modalKey]
+    	console.log("Pizza " + modalKey)
+    	// tamanho
+	    let size = seleciona('.pizzaInfo--size.selected').getAttribute('data-key')
+	    console.log("Tamanho " + size)
+	    // quantidade
+    	console.log("Quant. " + quantPizzas)
+        // preco
+        let price = seleciona('.pizzaInfo--actualPrice').innerHTML.replace('R$&nbsp;', '')
+    
+        // crie um identificador que junte id e tamanho
+	    // concatene as duas informacoes separadas por um símbolo, vc escolhe
+	    let identificador = pizzaJson[modalKey].id+'t'+size
+
+        // antes de adicionar verifique se ja tem aquele codigo e tamanho
+        // para adicionarmos a quantidade
+        let key = cart.findIndex( (item) => item.identificador == identificador )
+        console.log(key)
+
+        if(key > -1) {
+            // se encontrar aumente a quantidade
+            cart[key].qt += quantPizzas
+        } else {
+            // adicionar objeto pizza no carrinho
+            let pizza = {
+                identificador,
+                id: pizzaJson[modalKey].id,
+                size, // size: size
+                qt: quantPizzas,
+                price: parseFloat(price) // price: price
+            }
+            cart.push(pizza)
+            console.log(pizza)
+            console.log('Sub total R$ ' + (pizza.qt * pizza.price).toFixed(2))
+        }
+
+        fecharModal()
+        abrirCarrinho()
+        atualizarCarrinho()
+    })
+}
+
+const abrirCarrinho = () => {
+    console.log('Qtd de itens no carrinho ' + cart.length)
+    if(cart.length > 0) {
+        // mostrar o carrinho
+	    seleciona('aside').classList.add('show')
+        seleciona('header').style.display = 'flex' // mostrar barra superior
+    }
+
+    // exibir aside do carrinho no modo mobile
+    seleciona('.menu-openner').addEventListener('click', () => {
+        if(cart.length > 0) {
+            seleciona('aside').classList.add('show')
+            seleciona('aside').style.left = '0'
+        }
+    })
+}
+
+const fecharCarrinho = () => {
+    // fechar o carrinho com o botão X no modo mobile
+    seleciona('.menu-closer').addEventListener('click', () => {
+        seleciona('aside').style.left = '100vw' // usando 100vw ele ficara fora da tela
+        seleciona('header').style.display = 'flex'
+    })
+}
+
+const atualizarCarrinho = () => {
+    // exibir número de itens no carrinho
+	seleciona('.menu-openner span').innerHTML = cart.length
+	
+	// mostrar ou nao o carrinho
+	if(cart.length > 0) {
+
+		// mostrar o carrinho
+		seleciona('aside').classList.add('show')
+
+		// zerar meu .cart para nao fazer insercoes duplicadas
+		seleciona('.cart').innerHTML = ''
+
+        // crie as variaveis antes do for
+		let subtotal = 0
+		let desconto = 0
+		let total    = 0
+
+        // para preencher os itens do carrinho, calcular subtotal
+		for(let i in cart) {
+			// use o find para pegar o item por id
+			let pizzaItem = pizzaJson.find( (item) => item.id == cart[i].id )
+			console.log(pizzaItem)
+
+            // em cada item pegar o subtotal
+        	subtotal += cart[i].price * cart[i].qt
+            //console.log(cart[i].price)
+
+			// fazer o clone, exibir na telas e depois preencher as informacoes
+			let cartItem = seleciona('.models .cart--item').cloneNode(true)
+			seleciona('.cart').append(cartItem)
+
+			let pizzaSizeName = cart[i].size
+
+			let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+			// preencher as informacoes
+			cartItem.querySelector('img').src = pizzaItem.img
+			cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName
+			cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+
+			// selecionar botoes + e -
+			cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+				console.log('Clicou no botão mais')
+				// adicionar apenas a quantidade que esta neste contexto
+				cart[i].qt++
+				// atualizar a quantidade
+				atualizarCarrinho()
+			})
+
+			cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+				console.log('Clicou no botão menos')
+				if(cart[i].qt > 1) {
+					// subtrair apenas a quantidade que esta neste contexto
+					cart[i].qt--
+				} else {
+					// remover se for zero
+					cart.splice(i, 1)
+				}
+
+                (cart.length < 1) ? seleciona('header').style.display = 'flex' : ''
+
+				// atualizar a quantidade
+				atualizarCarrinho()
+			})
+
+			seleciona('.cart').append(cartItem)
+
+		} // fim do for
+
+		// fora do for
+		// calcule desconto 10% e total
+		//desconto = subtotal * 0.1
+		desconto = subtotal * 0
+		total = subtotal - desconto
+
+		// exibir na tela os resultados
+		// selecionar o ultimo span do elemento
+		seleciona('.subtotal span:last-child').innerHTML = formatoReal(subtotal)
+		seleciona('.desconto span:last-child').innerHTML = formatoReal(desconto)
+		seleciona('.total span:last-child').innerHTML    = formatoReal(total)
+
+	} else {
+		// ocultar o carrinho
+		seleciona('aside').classList.remove('show')
+		seleciona('aside').style.left = '100vw'
+	}
+}
+
+const finalizarCompra = () => {
+    seleciona('.cart--finalizar').addEventListener('click', () => {
+        console.log('Finalizar compra')
+        seleciona('aside').classList.remove('show')
+        seleciona('aside').style.left = '100vw'
+        seleciona('header').style.display = 'flex'
+    })
+}
+
+// /aula 06
+
 // MAPEAR pizzaJson para gerar lista de pizzas
 pizzaJson.map((item, index ) => {
     //console.log(item)
@@ -164,3 +337,10 @@ pizzaJson.map((item, index ) => {
 // mudar quantidade com os botoes + e -
 mudarQuantidade()
 // /aula 05
+
+// aula 06
+adicionarNoCarrinho()
+atualizarCarrinho()
+fecharCarrinho()
+finalizarCompra()
+// /aula 06
